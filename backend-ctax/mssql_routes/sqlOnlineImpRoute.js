53643a34e4db200a925dbec19009cebb922c65d5
hardik-agrl <hardik2001agarwal@gmail.com>
2025-04-15 11:29:47 +0530

    corrected the date filtration system

--- backend-ctax/mssql_routes/sqlOnlineImpRoute.js
+++ backend-ctax/mssql_routes/sqlOnlineImpRoute.js
@@ -6,17 +6,18 @@ const router = express.Router();
 
 
 router.get('/onlineImp', async (req, res) => {
-    const { from_date, to_date } = req.body;
+    const { from_date, to_date } = req.query;
+    console.log('Received:', from_date, to_date);
     try {
         const pool = await connectDB();
         let query = 'SELECT * FROM OnlineImport';
         let request = pool.request();
 
-        
         if (from_date && to_date) {
             query += ' WHERE created_at BETWEEN @from_date AND @to_date';
-            request.input('from_date', sql.DateTime, from_date)
-                   .input('to_date', sql.DateTime, to_date);
+            request.input('from_date', sql.DateTime, new Date(from_date + 'T00:00:00'));
+            request.input('to_date', sql.DateTime, new Date(to_date + 'T23:59:59'));
+
         }
 
         const result = await request.query(query);
@@ -25,5 +26,6 @@ router.get('/onlineImp', async (req, res) => {
         res.status(500).json({ message: 'Error fetching data', error });
     }
 });
+
 
 export default router;
